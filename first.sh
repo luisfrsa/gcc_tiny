cec7dc2ccb96017a8b178db44d0eb9ef6786b135
Luis Alves <luisfernandorsalves@gmail.com>
2017-11-14 16:07:55 -0200

    enviando tiger servidor

--- first.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd gcc-build;
-../gcc-src/configure --prefix=$(pwd)/../gcc-install --enable-languages=c,c++;
-make -j$(getconf _NPROCESSORS_ONLN);
-make install;
-../gcc-src/configure --prefix=$(pwd)/../gcc-install --disable-bootstrap --enable-languages=c,c++,tiny
-make -j$(getconf _NPROCESSORS_ONLN)
-make install
--- second.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-../gcc-src/configure --prefix=$(pwd)/../gcc-install --disable-bootstrap --enable-languages=c,c++,tiny
-make -j$(getconf _NPROCESSORS_ONLN)
-make install
